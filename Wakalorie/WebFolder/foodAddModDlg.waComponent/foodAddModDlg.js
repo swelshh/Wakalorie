b7f4e9976ee4f593fa4fd6ba9d324fba778c4baf
Welsh Harris <welsh.harris@corebitsdw.com>
2013-08-08 22:56:46 -0500

    componetized everything!

---
+++ Wakalorie/WebFolder/foodAddModDlg.waComponent/foodAddModDlg.js
@@ -0,0 +1,119 @@
+﻿/** 
+ * @fileOverview Dialog used to add or modify user foods 
+ * @author Welsh Harris
+ * @created 08/08/2013
+ *
+ * @name Wakalorie
+ * @copyright (c) 2013 CoreBits DataWorks LLC
+ * @license Released under the MIT license (included in distribution in MIT LICENSE.txt)
+ */
+ 
+(function Component (id) {// @lock
+
+// Add the code that needs to be shared between components here
+
+function constructor (id) {
+	"use strict";
+	
+	// @region beginComponentDeclaration// @startlock
+	var $comp = this;
+	this.name = 'foodAddModDlg';
+	// @endregion// @endlock
+
+
+	//-------------------------------------------------------------------------
+	//Component API
+	//-------------------------------------------------------------------------
+	var titleText = $$(getHtmlId("richText1")),
+		nameFld = $$(getHtmlId("textField1")),
+		caloriesFld = $$(getHtmlId("textField2")),
+		saveBtn = $$(getHtmlId("button5")),
+		cancelBtn = $$(getHtmlId("button4")),
+		foodSource = sources.food;
+	
+	//init
+	function initC() {
+		
+		//save button click event
+		WAF.addListener(saveBtn, "click", function(event) {
+			save();
+		});
+		
+		//cancel button click event
+		WAF.addListener(cancelBtn, "click", function(event) {
+			cancel();
+		});
+		
+		//attach an even to the calories field on the new food dialog so that if the user clicks the return key we will go ahead and save
+		$("#"+caloriesFld.id).keydown(function (event) {
+			if (event.which === 13) {
+				foodSource.calories = caloriesFld.getValue(); //without this it won't recognize if the value changed
+				save();
+			}
+		});
+	}
+	
+	//open the dialog to create a new food
+	function add() {
+		foodSource.addNewElement();
+		$comp.show();
+		titleText.setValue("New Food");
+		nameFld.focus();
+	}
+	
+	//open the dialog to modify an existing food
+	function modify() {
+		$comp.show();
+		titleText.setValue("Edit Food");
+		nameFld.focus();
+	}
+	
+	//user clicked the save button
+	function save() {
+		foodSource.save(async_save);
+		$comp.hide();
+	}
+	
+	//after saving set the qty to 1 and goto the qty var
+	function async_save(event) {
+		if (WAKL.err.async_ThereWasntAnError(event)) {
+			/* temporarily_disabled
+			WAKL.qtyAddArea.setAndGotoQty();
+			*/
+		}
+	}
+	
+	//user clicked the cancel button
+	function cancel() {
+		var isNew = foodSource.isNewElement();
+		
+		if (isNew) {
+			foodSource.removeCurrentReference();
+		}
+		$comp.hide();
+	}
+	
+	//--------------------
+	//public API
+	//--------------------
+	this.initC = initC;
+	this.add = add;
+	this.modify = modify;
+	
+	
+	this.load = function (data) {// @lock
+
+	// @region namespaceDeclaration// @startlock
+	// @endregion// @endlock
+
+	// eventHandlers// @lock
+
+	// @region eventManager// @startlock
+	// @endregion// @endlock
+
+	};// @lock
+
+
+}// @startlock
+return constructor;
+})();// @endlock
---
+++ Wakalorie/WebFolder/foodGrid.waComponent/foodGrid.js
@@ -0,0 +1,99 @@
+﻿/** 
+ * @fileOverview Grid for working with user foods
+ * @author Welsh Harris
+ * @created 08/08/2013
+ *
+ * @name Wakalorie
+ * @copyright (c) 2013 CoreBits DataWorks LLC
+ * @license Released under the MIT license (included in distribution in MIT LICENSE.txt)
+ */
+ 
+(function Component (id) {// @lock
+
+// Add the code that needs to be shared between components here
+
+function constructor (id) {
+	"use strict";
+	
+	// @region beginComponentDeclaration// @startlock
+	var $comp = this;
+	this.name = 'foodGrid';
+	// @endregion// @endlock
+
+
+	//-------------------------------------------------------------------------
+	//Component API
+	//-------------------------------------------------------------------------
+	var foodGrid = $$(getHtmlId("dataGrid1")),
+		addBtn = $$(getHtmlId("imageButton1")),
+		deleteBtn = $$(getHtmlId("imageButton2")),
+		searchText = $$(getHtmlId("textField1")),
+		foodSource = sources.food;
+
+	//init
+	function initC() {
+		
+		//add button click event
+		WAF.addListener(addBtn, "click", function(event) {
+			WAKL.foodAddModDlg.add();
+		});
+		
+		//delete button click event
+		WAF.addListener(deleteBtn, "click", function(event) {
+			removeFood();
+		});
+		
+		/* temporarily_disabled
+		//grid onRowClick event
+		WAF.addListener(foodGrid, "onRowClick", function(event) {
+			WAKL.qtyAddArea.setAndGotoQty();
+		});
+		*/
+		
+		//grid onRowDblClick event
+		WAF.addListener(foodGrid, "onRowDblClick", function(event) {
+			WAKL.foodAddModDlg.modify();
+		});
+			
+		//search bar on keyup event
+		WAF.addListener(searchText, "keyup", _.throttle(
+			function(event) {
+				search();
+			}, 
+			300, {leading: false})
+		);
+	}
+
+	//delete the current food
+	function removeFood() {
+		foodSource.removeCurrent(WAKL.err.async_ErrCheckOnly);
+	}
+	
+	//do a contains search when user types in the search bar
+	function search() {
+		var searchVal = "*"+searchText.getValue()+"*";
+		foodSource.query("name = :1", WAKL.err.async_ErrCheckOnly, {params:[searchVal]});
+	}
+
+	//--------------------
+	//public API
+	//--------------------
+	this.initC = initC;
+
+
+	this.load = function (data) {// @lock
+
+	// @region namespaceDeclaration// @startlock
+	// @endregion// @endlock
+
+	// eventHandlers// @lock
+
+	// @region eventManager// @startlock
+	// @endregion// @endlock
+
+	};// @lock
+
+
+}// @startlock
+return constructor;
+})();// @endlock
